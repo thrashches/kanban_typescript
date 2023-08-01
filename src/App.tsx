bdbc7a165287c65bfc6724999aca56c7584a3846
thrashches <thrashches@yandex.ru>
2023-08-02 01:18:54 +0300

    Small data mock

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Layout from "./components/Layout/Layout";
 import {ITask} from "./data/types";
+import {initialTasks} from "./data/dataMock";
+
 
 function App() {
-    const [tasks, setTasks] = useState<ITask[]>([]);
+    const [tasks, setTasks] = useState<ITask[]>(initialTasks);
     return (
         <div className="App">
             <Header/>
---
+++ src/data/dataMock.ts
@@ -0,0 +1,92 @@
+import {ITask} from "./types";
+
+const initialTasks: ITask[] = [
+    {
+        status: "backlog",
+        title: "Login page – performance issues",
+        description: "Login page – performance issues",
+        id: 1
+    },
+    {
+        status: "backlog",
+        title: "Sprint bugfix",
+        description: "Sprint bugfix",
+        id: 2
+    },
+    {
+        status: "ready",
+        title: "Shop page – performance issues",
+        description: "Shop page – performance issues",
+        id: 3
+    },
+    {
+        status: "ready",
+        title: "Checkout bugfix",
+        description: "Checkout bugfix",
+        id: 4
+    },
+    {
+        status: "ready",
+        title: "Checkout bugfix",
+        description: "Checkout bugfix",
+        id: 5
+    },
+    {
+        status: "ready",
+        title: "Checkout bugfix",
+        description: "Checkout bugfix",
+        id: 6
+    },
+    {
+        status: "ready",
+        title: "Checkout bugfix",
+        description: "Checkout bugfix",
+        id: 7
+    },
+    {
+        status: "ready",
+        title: "Shop bug2",
+        description: "Checkout bugfix",
+        id: 8
+    },
+    {
+        status: "ready",
+        title: "Shop bug1",
+        description: "Checkout bugfix",
+        id: 9
+    },
+    {
+        status: "ready",
+        title: "Shop bug3",
+        description: "Checkout bugfix",
+        id: 10
+    },
+    {
+        status: "ready",
+        title: "Shop bug4",
+        description: "Checkout bugfix",
+        id: 11
+    },
+    {
+        status: "inProgress",
+        title: "User page – performance issues",
+        description: "Checkout bugfix",
+        id: 12
+    },
+    {
+        status: "inProgress",
+        title: "Auth bugfix",
+        description: "Checkout bugfix",
+        id: 13
+    },
+    {
+        status: "finished",
+        title: "Main page – performance issues",
+        description: "Checkout bugfix",
+        id: 14
+    }
+]
+
+export {
+    initialTasks,
+}
